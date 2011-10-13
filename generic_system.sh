f9a23a8e8d49324431d69ccd51b4cb6ccee1b504
Brian Morris <brian.morris.personal@gmail.com>
2011-10-12 23:52:49 -0700

    First Commit

---
+++ generic_system.sh
@@ -0,0 +1,323 @@
+#!/bin/bash
+function check_install {
+    if [ -z "`which "$1" 2>/dev/null`" ]
+    then
+        executable=$1
+        shift
+        while [ -n "$1" ]
+        do
+            DEBIAN_FRONTEND=noninteractive apt-get -qq -y install "$1"
+            print_info "$1 installed for $executable"
+            shift
+        done
+    else
+        print_warn "$2 already installed"
+    fi
+}
+
+function check_remove {
+    if [ -n "`which "$1" 2>/dev/null`" ]
+    then
+        DEBIAN_FRONTEND=noninteractive apt-get -qq -y remove --purge "$2"
+        print_info "$2 removed"
+    else
+        print_warn "$2 is not installed"
+    fi
+}
+
+function get_password() {
+    # Check whether our local salt is present.
+    SALT=/var/lib/random_salt
+    if [ ! -f "$SALT" ]
+    then
+        head -c 512 /dev/urandom > "$SALT"
+        chmod 400 "$SALT"
+    fi
+    password=`(cat "$SALT"; echo $1) | md5sum | base64`
+    echo ${password:0:13}
+}
+
+function install_dash {
+    check_install dash dash
+    rm -f /bin/sh
+    ln -s dash /bin/sh
+}
+
+function print_info {
+    echo -n -e '\e[1;36m'
+    echo -n $1
+    echo -e '\e[0m'
+}
+
+function print_warn {
+    echo -n -e '\e[1;33m'
+    echo -n $1
+    echo -e '\e[0m'
+}
+
+function check_sanity {
+    # Do some sanity checking.
+    if [ $(/usr/bin/id -u) != "0" ]
+    then
+        die 'Must be run by root user'
+    fi
+
+    if [ ! -f /etc/debian_version ]
+    then
+        die "Distribution is not supported"
+    fi
+}
+
+function die {
+    echo "ERROR: $1" > /dev/null 1>&2
+    exit 1
+}
+
+function remove_unneeded {
+    # Some Debian have portmap installed. We don't need that.
+    check_remove /sbin/portmap portmap
+
+    # Remove rsyslogd, which allocates ~30MB privvmpages on an OpenVZ system,
+    # which might make some low-end VPS inoperatable. We will do this even
+    # before running apt-get update.
+    check_remove /usr/sbin/rsyslogd rsyslog
+
+    # Other packages that seem to be pretty common in standard OpenVZ
+    # templates.
+    check_remove /usr/sbin/apache2 'apache2*'
+    check_remove /usr/sbin/named bind9
+    check_remove /usr/sbin/smbd 'samba*'
+    check_remove /usr/sbin/nscd nscd
+
+    # Need to stop sendmail as removing the package does not seem to stop it.
+    if [ -f /usr/lib/sm.bin/smtpd ]
+    then
+        service sendmail stop
+        check_remove /usr/lib/sm.bin/smtpd 'sendmail*'
+    fi
+}
+
+function install_syslogd {
+    # We just need a simple vanilla syslogd. Also there is no need to log to
+    # so many files (waste of fd). Just dump them into
+    # /var/log/(cron/mail/messages)
+    check_install /usr/sbin/syslogd inetutils-syslogd
+    service inetutils-syslogd stop
+
+    for file in /var/log/*.log /var/log/mail.* /var/log/debug /var/log/syslog
+    do
+        [ -f "$file" ] && rm -f "$file"
+    done
+    for dir in fsck news
+    do
+        [ -d "/var/log/$dir" ] && rm -rf "/var/log/$dir"
+    done
+
+    cat > /etc/syslog.conf <<END
+*.*;mail.none;cron.none -/var/log/messages
+cron.*                  -/var/log/cron
+mail.*                  -/var/log/mail
+END
+
+    [ -d /etc/logrotate.d ] || mkdir -p /etc/logrotate.d
+    cat > /etc/logrotate.d/inetutils-syslogd <<END
+/var/log/cron
+/var/log/mail
+/var/log/messages {
+   rotate 4
+   weekly
+   missingok
+   notifempty
+   compress
+   sharedscripts
+   postrotate
+      /etc/init.d/inetutils-syslogd reload >/dev/null
+   endscript
+}
+END
+
+    service inetutils-syslogd start
+}
+
+function install_dash {
+    check_install dash dash
+    rm -f /bin/sh
+    ln -s dash /bin/sh
+}
+
+function install_dropbear {
+    check_install dropbear dropbear
+    check_install /usr/sbin/xinetd xinetd
+
+    # Disable SSH
+    touch /etc/ssh/sshd_not_to_be_run
+    service ssh stop
+
+    # Enable dropbear to start. We are going to use xinetd as it is just
+    # easier to configure and might be used for other things.
+    cat > /etc/xinetd.d/dropbear <<END
+service ssh
+{
+    socket_type     = stream
+    only_from       = 0.0.0.0
+    wait            = no
+    user            = root
+    protocol        = tcp
+    server          = /usr/sbin/dropbear
+    server_args     = -i
+    disable         = no
+}
+END
+    service xinetd restart
+}
+
+function set_timezone {
+     cat > /etc/timezone <<END
+US/Pacific
+END
+     dpkg-reconfigure --frontend noninteractive tzdata
+}
+
+function update_upgrade {
+     print_info "apt-get update and upgrade now running"
+    # Run through the apt-get update/upgrade first. This should be done before
+    # we try to install any package
+    apt-get -qq -y update
+    apt-get -qq -y upgrade
+}
+
+function install_common {
+     apt-get install htop unzip zip curl python-software-properties nano
+}
+
+function install_nginxphp {
+     print_info "Starting Nginx/PHP5/PHP5 FPM Installation"
+     check_install nginx nginx
+     nginx=stable
+     add-apt-repository ppa:nginx/$nginx
+     apt-get install -qq nginx php5-fpm php5-mysql php5-curl php5-gd php5-idn php-pear php5-imagick php5-imap php5-mcrypt php5-memcache php5-ming php5-ps php5-pspell php5-recode php5-snmp php5-sqlite php5-tidy php5-xmlrpc php5-xsl
+
+service php5-fpm stop
+service nginx stop
+
+cat > /etc/nginx/conf.d/lowendbox.conf <<END
+server_names_hash_bucket_size 64;
+END
+
+cat > /etc/php5/fpm/pool.d/www.conf <<END
+listen = 127.0.0.1:9000
+user = www-data
+group = www-data
+pm = dynamic
+pm.max_children = 10
+pm.start_servers = 3
+pm.min_spare_servers = 3
+pm.max_spare_servers = 5
+pm.max_requests = 500
+pm.status_path = /status
+ping.path = /ping
+ping.response = pong
+slowlog = /var/log/php-fpm.log.slow
+chdir = /var/www
+END
+
+
+ 
+}
+
+function install_mysqlserver {
+    # Install the MySQL packages
+    check_install mysqld mysql-server
+
+    # Install a low-end copy of the my.cnf to disable InnoDB, and then delete
+    # all the related files.
+    service mysql stop
+    rm -f /var/lib/mysql/ib*
+    cat > /etc/mysql/conf.d/lowendbox.cnf <<END
+[mysqld]
+key_buffer = 8M
+query_cache_size = 0
+skip-innodb
+END
+    service mysql start
+
+    # Generating a new password for the root user.
+    passwd=`get_password root@mysql`
+    mysqladmin password "$passwd"
+    cat > ~/.my.cnf <<END
+[client]
+user = root
+password = $passwd
+END
+    chmod 600 ~/.my.cnf
+}
+
+function install_mysqlclient {
+         check_install mysql mysql-client
+}
+
+function make_directories {
+     mkdir -p /storage/web/vhosts/
+     chown -R www-data:www-data /storage/web/
+
+}
+
+
+function start_services {
+     service php5-fpm start
+     service nginx start
+}
+
+########################################################################
+# START OF PROGRAM
+########################################################################
+export PATH=/bin:/usr/bin:/sbin:/usr/sbin
+
+check_sanity
+case "$1" in
+system)
+     remove_unneeded
+     sleep 2
+     update_upgrade
+     sleep 2
+     install_dash
+     sleep 2
+     install_syslogd
+     sleep 2
+     install_dropbear
+     sleep 2
+     install_common
+     sleep 2
+     set_timezone
+     sleep 2
+     update_upgrade
+     sleep 2
+    ;;
+webserver)
+     install_nginxphp
+     sleep 2
+     update_upgrade
+     sleep 2
+     make_directories
+     sleep 2
+     start_services
+     sleep 2
+     ;;
+mysql-server)
+     install_mysqlserver
+     sleep 2
+     ;;
+mysql-client)
+     install_mysqlclient
+     sleep 2
+     ;;
+*)
+    echo 'Usage:' `basename $0` '[option]'
+    echo 'Available option:'
+    for option in system webserver mysql-server mysql-client
+    do
+        echo '  -' $option
+    done
+    ;;
+esac
+
